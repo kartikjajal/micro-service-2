823321f16615d71fef1dc37b5941115e7bfe8b3b
kartik <kartik@ics-global.in>
2018-09-04 17:36:24 +0530

    zipkin and admin access implemented

--- src/main/java/service/discover/server/MicroService2Application.java
+++ src/main/java/service/discover/server/MicroService2Application.java
@@ -14,5 +14,5 @@ public class MicroService2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroService2Application.class, args);
-	}
+	}	
 }
--- src/main/java/service/discover/server/Service2.java
+++ src/main/java/service/discover/server/Service2.java
@@ -1,5 +1,7 @@
 package service.discover.server;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +14,34 @@ public class Service2 {
 	@Autowired
 	private Service1 service1;
 	
+	private static final Logger LOG = Logger.getLogger(Service1.class.getName());
+	
 	@Value("${server.port}")
     private int serverPort;
 
 	@RequestMapping("/welcome")
 	public String welcome() {
+		LOG.info("Before welcome()");
 		return "welcome-to-service-2 with "+serverPort;
 	}
 	
 	@RequestMapping("/service1")
 	public String getService1() {
-		return service1.getService1();
+		LOG.info("Before getService1()");
+		String temp = service1.getService1();
+		LOG.info("After getService1()");
+		return temp;
 	}
 	
 	@RequestMapping("/common")
 	public String common() {
 		return "welcome-to-common-service-2 with "+serverPort;
 	}
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		return "welcome-to-admin-service-2 with "+serverPort;
+	}
+
 	
 }
